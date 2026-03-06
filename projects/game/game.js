9b500f6b14019444906d9fbb6547ec923d4783ef
Sebastian Seguin <sebastianmseguin@gmail.com>
2026-03-06 11:43:32 -0500

    added dragon class and house location

--- projects/game/game.js
+++ projects/game/game.js
@@ -2,6 +2,13 @@ let gameActive = true; //this variable is required.
                        //to stop the game, set it to false.
 
 //Declare your other global variables here
+class dragon {
+    constructor() {
+        this.name = this.name;
+        this.ascii = this.ascii;
+    }
+}
+
 
 asciiDragon = `
         _^__^_
@@ -14,17 +21,38 @@ ___/^^^^^^^^/
 asciiDog = `
             ^  ^
  ^  ^      | \' \'|
-\\ | /      |  * |
- \\|/_______| \\_/|
-  --|           |
+\\ | /      |  * | /
+ \\|/_______| \\_/| --
+  --|           | \\
     |___________|
       | |   | |
 `;
+
+petDragon = new dragon("Pet Dragon", asciiDragon);
+doggo = new dragon("Doggo", asciiDog);
 
 //If you need, add any "helper" functions here
 
 
 //Make one function for each location
+function house() {
+    clear();
+    print("\nYou are in your house!");
+    print("\nWhere do you want to go next? Say one of these choices: \n\tlocationA\n\tlocationB");
+
+    function processInput(input) {
+        if (input.toLowerCase() === "locationb") {
+            locationB();
+        } else if (input.toLowerCase() === "locationa") {
+            locationA();
+        } else {
+            stayHere();
+            waitThenCall(house);
+        }
+    }
+    waitForInput(processInput);
+}
+
 function locationA() {
     clear();
     print("\nYou are in location A!");
@@ -63,12 +91,12 @@ function locationB() {
 //very start. For this simple example, any input will bring you
 //to locationA
 function start(){
-    print("Welcome to my game! Press any key to start");
+    print("Welcome to my game! Press enter to start");
     printAscii(asciiDragon);
     printAscii(asciiDog);
 
     function processInput(input){
-            locationA();
+            house();
     }
 
     waitForInput(processInput);
